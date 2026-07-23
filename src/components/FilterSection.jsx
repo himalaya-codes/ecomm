1458524bc6068897d668b4b1b77d771edaf23335
Himalaya Lakhera <himalaya.lak@gmail.com>
2026-07-23 23:12:12 +0530

    filter category

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/FilterContext";
 
 const FilterSection = () => {
-  return <div>FilterSection</div>;
+  const {
+    filter: { text },
+    updateFilterValue,
+    all_products,
+  } = useFilterContext();
+
+  const getUniqueData = (data, property) => {
+    let newVal = data.map((curElem) => {
+      return curElem[property];
+    });
+  };
+
+  // unique category data
+  const categoryOnlyData = getUniqueData(all_products, "category");
+
+  return (
+    <Section className="filter_section">
+      <div className="form_wrap">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            className="form-control"
+            type="text"
+            name="text"
+            placeholder="Search Product"
+            value={text}
+            onChange={updateFilterValue}
+          />
+        </form>
+      </div>
+    </Section>
+  );
 };
 
 export default FilterSection;
+
+const Section = styled.section``;
--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -5,7 +5,9 @@ import { IoGridOutline } from "react-icons/io5";
 import { useFilterContext } from "../context/FilterContext";
 
 const Sort = () => {
-  const { grid_view, setGridView, setListView } = useFilterContext();
+  const { grid_view, setGridView, setListView, filter_products, sorting } =
+    useFilterContext();
+  console.log(filter_products);
   return (
     <Div>
       <div className="row">
@@ -26,10 +28,25 @@ const Sort = () => {
           </div>
         </div>
         <div className="col-md-4">
-          <div className="products_data">products available</div>
+          <div className="products_data">
+            <p>{`${filter_products.length} products available`}</p>
+          </div>
         </div>
         <div className="col-md-4">
-          <div className="sort_select">Sort Selection</div>
+          <div className="sort_select">
+            <form action="#">
+              <select
+                name="sort"
+                id="sort"
+                onChange={(e) => sorting(e.target.value)}
+              >
+                <option value="lowest">Price: Lowest</option>
+                <option value="highest">Price: Highest</option>
+                <option value="a-z">Name: A to Z</option>
+                <option value="z-a">Name: Z to A</option>
+              </select>
+            </form>
+          </div>
         </div>
       </div>
     </Div>
